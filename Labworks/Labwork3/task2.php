776b4c3fcef6e34c19c1a747190c451b385989e1
quanganh2001 <cocoi5659@gmail.com>
2023-09-25 12:23:12 +0700

    write a PHP script to display the following strings

---
+++ Labworks/Labwork3/task2.php
@@ -0,0 +1,7 @@
+<?php
+	$string1 = 'Tomorrow I \'ll study PHP server-side scripting language.';
+	$string2 = 'Can you check if this command is correct : del c:\\*.**';
+	
+	echo $string1 . "\n";
+	echo $string2;
+?>
